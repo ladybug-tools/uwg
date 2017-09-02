1b8487ea69883d7cd1f5ebf85c4d4a5461030583
Saeran Vasanthakumar <saeranv@gmail.com>
2017-09-02 00:05:24 -0400

    adding default None for force class instance variables

--- UWG/forcing.py
+++ UWG/forcing.py
@@ -5,21 +5,36 @@ class Forcing (object):
 
     """
 
-    def __init__(self,staTemp,weather):
+    def __init__(self,staTemp=None,weather=None):
         # weather: Weather obj
         # staTemp: list of hourly air temperature for simulation period
-        self.deepTemp = sum(staTemp)/float(len(staTemp))    # deep soil temperature (K)
-        self.waterTemp = sum(staTemp)/float(len(staTemp))   # ground water temp, set to temp at 2m
-        self.infra = weather.staInfra     # horizontal Infrared Radiation Intensity (W m-2)
-        self.uDir = weather.staUdir       # wind direction
-        self.hum  = weather.staHum        # specific humidty (kg kg-1)
-        self.pres = weather.staPres       # Pressure (Pa)
-        self.temp = weather.staTemp       # air temperature (C)
-        self.rHum = weather.staRhum       # Relative humidity (%)
-        self.dir = weather.staDir         #  normal solar direct radiation (W m-2)
-        self.dif = weather.staDif         # horizontal solar diffuse radiation (W m-2)
-        self.prec = map(lambda p: p/3.6e6, weather.staRobs) #  Precipitation (mm h-1)
-        self.wind = weather.staUmod       # wind speed (m s-1)
+        # Define default values for instance variables, when the type can be mutable:
+        if staTemp==None and weather==None:
+            self.deepTemp = None                # deep soil temperature (K)
+            self.waterTemp = None               # ground water temp, set to temp at 2m
+            self.infra = None                   # horizontal Infrared Radiation Intensity (W m-2)
+            self.uDir = None                    # wind direction
+            self.hum  = None                    # specific humidty (kg kg-1)
+            self.pres = None                    # Pressure (Pa)
+            self.temp = None                    # air temperature (C)
+            self.rHum = None                    # Relative humidity (%)
+            self.dir = None                     #  normal solar direct radiation (W m-2)
+            self.dif = None                     # horizontal solar diffuse radiation (W m-2)
+            self.prec = None                    #  Precipitation (mm h-1)
+            self.wind = None                    # wind speed (m s-1)
+        else:
+            self.deepTemp = sum(staTemp)/float(len(staTemp))
+            self.waterTemp = sum(staTemp)/float(len(staTemp))
+            self.infra = weather.staInfra
+            self.uDir = weather.staUdir
+            self.hum  = weather.staHum
+            self.pres = weather.staPres
+            self.temp = weather.staTemp
+            self.rHum = weather.staRhum
+            self.dir = weather.staDir
+            self.dif = weather.staDif
+            self.prec = map(lambda p: p/3.6e6, weather.staRobs)
+            self.wind = weather.staUmod
 
     def __repr__(self):
         return "Forcing: deepT={a}, waterT={b}".format(
