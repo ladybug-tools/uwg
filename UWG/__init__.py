d052a2bc8dff83bcb102c34c55f164b53b0451bd
Saeran Vasanthakumar <saeranv@gmail.com>
2017-09-14 00:14:18 -0400

    adding .gitignore

--- UWG/__init__.py
+++ UWG/__init__.py
@@ -1,4 +1,26 @@
 """Urban Weather Generator Library."""
-__all__ = ["UWG", "utilities", "material"]
 
-#import UWG, utilities, material
+import sys
+import os
+
+__all__ = [
+    "UWG",
+    "utilities",
+    "material",
+    "element",
+    "building",
+    "BEMDef",
+    "forcing",
+    "param",
+    "psychrometrics",
+    "schdef",
+    "simparam",
+    "UCMDef",
+    "urbflux",
+    "weather",
+    ]
+
+if __name__ == '__main__':
+    # path to the real library
+    path_to_up_dir = os.path.join(os.path.dirname(__file__),'..')
+    sys.path.insert(0, path_to_up_dir)
