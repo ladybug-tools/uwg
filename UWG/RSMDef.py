216385e5198f9930d649e9e2eb39d968701fe6f5
Saeran Vasanthakumar <saeranv@gmail.com>
2018-01-14 18:15:24 -0500

    Finished translating DissipationBougeault function in RSMDef. Needs testing, as the code is very difficult to read/understand

--- UWG/RSMDef.py
+++ UWG/RSMDef.py
@@ -123,7 +123,7 @@ class RSMDef(object):
         return "RSM: obstacle ht={a}".format(
             a=self.height
             )
-    def is_near_zero(self,num,eps=1e-10):
+    def is_near_zero(self,num,eps=1e-16):
         return abs(float(num)) < eps
 
     def load_z_meso(self,z_meso_path):
@@ -175,7 +175,7 @@ class RSMDef(object):
 
         # Ref: The UWG (2012), Eq. (5)
         # compute diffusion coefficient
-        cd,ustarRur = DiffusionCoefficient(self.densityProfC[0], \
+        cd,ustarRur = self.DiffusionCoefficient(self.densityProfC[0], \
             self.z, self.dz, self.z0r, self.disp, \
             self.tempProf[0], rural.sens, self.nzref, forc.wind, \
             self.tempProf, parameter)
@@ -197,188 +197,183 @@ class RSMDef(object):
                 (self.z(self.nzref)+self.dz(self.nzref)/2);
         """
 
-def DiffusionEquation(nz,dt,co,da,daz,cd,dz):
-    # Reference?
-    """
-    cddz = zeros(nz+2,1);
-    a = zeros(nz,3);
-    c = zeros(nz,1);
-
-    %--------------------------------------------------------------------------
-    cddz(1)= daz(1)*cd(1)/dz(1);
-    for iz=2:nz
-       cddz(iz) = 2.*daz(iz)*cd(iz)/(dz(iz)+dz(iz-1));
-    end
-    cddz(nz+1) = daz(nz+1)*cd(nz+1)/dz(nz);
-    %--------------------------------------------------------------------------
-    a(1,1)=0.;
-    a(1,2)=1.;
-    a(1,3)=0.;
-    c(1)=co(1);
-    for iz=2:nz-1
-       dzv=dz(iz);
-       a(iz,1)=-cddz(iz)*dt/dzv/da(iz);
-       a(iz,2)=1+dt*(cddz(iz)+cddz(iz+1))/dzv/da(iz);
-       a(iz,3)=-cddz(iz+1)*dt/dzv/da(iz);
-       c(iz)  =co(iz);
-    end
-    a(nz,1)=-1.;
-    a(nz,2)=1.;
-    a(nz,3)=0.;
-    c(nz)  =0;
-    %--------------------------------------------------------------------------
-    co = Invert (nz,a,c);
-    """
-    return co
-
-
-
-def DiffusionCoefficient(rho,z,dz,z0,disp,tempRur,heatRur,nz,uref,th,parameter):
-
-    # Initialization
-    Kt = [0 for x in xrange(nz+1)]
-    ws = [0 for x in xrange(nz)]
-    te = [0 for x in xrange(nz)]
-    # Friction velocity (Louis 1979)
-    ustar = parameter.vk * uref/math.log((10.-disp)/z0)
-
-
-    # Monin-Obukhov length
-    lengthRur = max(-rho*parameter.cp*ustar**3*tempRur/parameter.vk/parameter.g/heatRur,-50.)
-
-    # Unstable conditions
-    if heatRur > 1e-2:
-        # Convective velocity scale
-        wstar = (parameter.g*heatRur*parameter.dayBLHeight/rho/parameter.cp/tempRur)**(1/3.)
-        # Wind profile function
-        phi_m = (1-8.*0.1*parameter.dayBLHeight/lengthRur)**(-1./3.)
-
-        for iz in xrange(nz):
-            # Mixed-layer velocity scale
-            ws[iz] = (ustar**3 + phi_m*parameter.vk*wstar**3*z[iz]/parameter.dayBLHeight)**(1/3.)
-            # TKE approximation
-            te[iz] = max(ws[iz]**2., 0.01)
-
-    else: # Stable and neutral conditions
-        for iz in xrange(nz):
-            # TKE approximation
-            te[iz] = max(ustar**2.,0.01)
-
-    # lenght scales (l_up, l_down, l_k, l_eps)
-
-    dlu,dld = DissipationBougeault(parameter.g,nz,z,dz,te,th)
-    """
-    [dld,dls,dlk]= LengthBougeault(nz,dld,dlu,z);
-    % Boundary-layer diffusion coefficient
-    for iz=1:nz
-       Kt(iz) = 0.4*dlk(iz)*sqrt(te(iz));
-    end
-    Kt(nz+1) = Kt(nz);
-    """
-    return Kt, ustar
-
-
-def DissipationBougeault(g,nz,z,dz,te,pt):
-
-    dlu = [0 for x in xrange(nz)]
-    dld = [0 for x in xrange(nz)]
-
-    for iz in xrange(nz):
-        zup=0.
-        dlu[iz] = z[nz+1] - z[iz] - dz[iz]/2.
-        zzz=0.
-        zup_inf=0.
-        beta=g/pt[iz]
-
-        for izz in xrange(nz-1):
-           dzt=(dz[izz+1]+dz[izz])/2.
-           zup=zup-beta*pt[iz]*dzt
-           zup=zup+beta*(pt[izz+1]+pt[izz])*dzt/2.
-           zzz=zzz+dzt
-
-           #ge = greater than or equal for vector
-           """
-           if (lt(te[iz],zup) and ge(te(iz),zup_inf))
-             bbb=(pt(izz+1)-pt(izz))/dzt;
-             if ne(bbb,0)
-                tl=(-beta*(pt(izz)-pt(iz))+...
-                sqrt( max(0.,(beta*(pt(izz)-pt(iz)))^2.+...
-                2.*bbb*beta*(te(iz)-zup_inf))))/bbb/beta;
-             else
-             tl=(te(iz)-zup_inf)/(beta*(pt(izz)-pt(iz)));
-             end
-             dlu(iz)=max(1.,zzz-dzt+tl);
-           end
-           zup_inf=zup;
-        end
-        zdo=0.;
-        zdo_sup=0.;
-        dld(iz)=z(iz)+dz(iz)/2.;
-        zzz=0.;
-        for izz=iz:-1:2
-            dzt=(dz(izz-1)+dz(izz))/2.;
-            zdo=zdo+beta*pt(iz)*dzt;
-            zdo=zdo-beta*(pt(izz-1)+pt(izz))*dzt/2.;
-            zzz=zzz+dzt;
-            if (lt(te(iz),zdo) && ge(te(iz),zdo_sup))
-               bbb=(pt(izz)-pt(izz-1))/dzt;
-               if ne(bbb,0.)
-                 tl=(beta*(pt(izz)-pt(iz))+...
-                 sqrt( max(0.,(beta*(pt(izz)-pt(iz)))^2.+...
-                 2.*bbb*beta*(te(iz)-zdo_sup))))/bbb/beta;
-               else
-                 tl=(te(iz)-zdo_sup)/(beta*(pt(izz)-pt(iz)));
-               end
-               dld(iz)=max(1.,zzz-dzt+tl);
-            end
-            zdo_sup=zdo;
+    def DiffusionEquation(self,nz,dt,co,da,daz,cd,dz):
+        # Reference?
         """
-    return dlu,dld
+        cddz = zeros(nz+2,1);
+        a = zeros(nz,3);
+        c = zeros(nz,1);
 
-"""
-function [dld,dls,dlk] = LengthBougeault(nz,dld,dlu,z)
+        %--------------------------------------------------------------------------
+        cddz(1)= daz(1)*cd(1)/dz(1);
+        for iz=2:nz
+           cddz(iz) = 2.*daz(iz)*cd(iz)/(dz(iz)+dz(iz-1));
+        end
+        cddz(nz+1) = daz(nz+1)*cd(nz+1)/dz(nz);
+        %--------------------------------------------------------------------------
+        a(1,1)=0.;
+        a(1,2)=1.;
+        a(1,3)=0.;
+        c(1)=co(1);
+        for iz=2:nz-1
+           dzv=dz(iz);
+           a(iz,1)=-cddz(iz)*dt/dzv/da(iz);
+           a(iz,2)=1+dt*(cddz(iz)+cddz(iz+1))/dzv/da(iz);
+           a(iz,3)=-cddz(iz+1)*dt/dzv/da(iz);
+           c(iz)  =co(iz);
+        end
+        a(nz,1)=-1.;
+        a(nz,2)=1.;
+        a(nz,3)=0.;
+        c(nz)  =0;
+        %--------------------------------------------------------------------------
+        co = Invert (nz,a,c);
+        """
+        return co
 
-    dlg = zeros(nz,1);
-    dls = zeros(nz,1);
-    dlk = zeros(nz,1);
-    for iz=1:nz
-        dlg(iz)=(z(iz)+z(iz+1))/2.;
+
+
+    def DiffusionCoefficient(self,rho,z,dz,z0,disp,tempRur,heatRur,nz,uref,th,parameter):
+
+        # Initialization
+        Kt = [0 for x in xrange(nz+1)]
+        ws = [0 for x in xrange(nz)]
+        te = [0 for x in xrange(nz)]
+        # Friction velocity (Louis 1979)
+        ustar = parameter.vk * uref/math.log((10.-disp)/z0)
+
+
+        # Monin-Obukhov length
+        lengthRur = max(-rho*parameter.cp*ustar**3*tempRur/parameter.vk/parameter.g/heatRur,-50.)
+
+        # Unstable conditions
+        if heatRur > 1e-2:
+            # Convective velocity scale
+            wstar = (parameter.g*heatRur*parameter.dayBLHeight/rho/parameter.cp/tempRur)**(1/3.)
+            # Wind profile function
+            phi_m = (1-8.*0.1*parameter.dayBLHeight/lengthRur)**(-1./3.)
+
+            for iz in xrange(nz):
+                # Mixed-layer velocity scale
+                ws[iz] = (ustar**3 + phi_m*parameter.vk*wstar**3*z[iz]/parameter.dayBLHeight)**(1/3.)
+                # TKE approximation
+                te[iz] = max(ws[iz]**2., 0.01)
+
+        else: # Stable and neutral conditions
+            for iz in xrange(nz):
+                # TKE approximation
+                te[iz] = max(ustar**2.,0.01)
+
+        # lenght scales (l_up, l_down, l_k, l_eps)
+
+        dlu,dld = self.DissipationBougeault(parameter.g,nz,z,dz,te,th)
+        print dlu, dld
+        """
+        [dld,dls,dlk]= LengthBougeault(nz,dld,dlu,z);
+        % Boundary-layer diffusion coefficient
+        for iz=1:nz
+           Kt(iz) = 0.4*dlk(iz)*sqrt(te(iz));
+        end
+        Kt(nz+1) = Kt(nz);
+        """
+        return Kt, ustar
+
+
+
+    def DissipationBougeault(self,g,nz,z,dz,te,pt):
+
+        dlu = [0 for x in xrange(nz)]
+        dld = [0 for x in xrange(nz)]
+
+        for iz in xrange(nz):
+            zup=0.
+            dlu[iz] = z[nz+1] - z[iz] - dz[iz]/2.
+            zzz=0.
+            zup_inf=0.
+            beta=g/pt[iz]
+
+            for izz in xrange(nz-1):
+                dzt=(dz[izz+1]+dz[izz])/2.
+                zup=zup-beta*pt[iz]*dzt
+                zup=zup+beta*(pt[izz+1]+pt[izz])*dzt/2.
+                zzz=zzz+dzt
+
+                if (te[iz]<zup) and ((te[iz]>zup_inf) or self.is_near_zero(te[iz]-zup_inf)):
+                    bbb=(pt[izz+1]-pt[izz])/dzt
+                    if not self.is_near_zero(bbb-0.):
+                        tl=(-beta*(pt[izz]-pt[iz])+ \
+                        math.sqrt( max(0.,(beta*(pt[izz]-pt[iz]))**2.+ \
+                        2.*bbb*beta*(te[iz]-zup_inf))))/bbb/beta
+                    else:
+                        tl=(te[iz]-zup_inf)/(beta*(pt[izz]-pt[iz]))
+                    dlu[iz]=max(1.,zzz-dzt+tl)
+                zup_inf=zup
+            zdo=0.
+            zdo_sup=0.
+            dld[iz]=z[iz]+dz[iz]/2.
+            zzz=0.
+            for izz in reversed(range(iz)[1:]):
+                dzt=(dz[izz-1]+dz[izz])/2.
+                zdo=zdo+beta*pt[iz]*dzt
+                zdo=zdo-beta*(pt[izz-1]+pt[izz])*dzt/2.
+                zzz=zzz+dzt
+                if (te[iz]<zdo) and ((te[iz]>zdo_sup) or self.is_near_zero(te[iz]-zdo_sup)):
+                    bbb=(pt[izz]-pt[izz-1])/dzt
+                    if not self.is_near_zero(bbb-0.):
+                        tl=(beta*(pt[izz]-pt[iz])+ \
+                            math.sqrt( max(0.,(beta*(pt[izz]-pt[iz]))**2.+ \
+                            2.*bbb*beta*(te[iz]-zdo_sup))))/bbb/beta
+                    else:
+                        tl=(te[iz]-zdo_sup)/(beta*(pt[izz]-pt[iz]))
+                    dld[iz]=max(1.,zzz-dzt+tl)
+                zdo_sup=zdo
+
+        return dlu,dld
+
+    """
+    function [dld,dls,dlk] = LengthBougeault(nz,dld,dlu,z)
+
+        dlg = zeros(nz,1);
+        dls = zeros(nz,1);
+        dlk = zeros(nz,1);
+        for iz=1:nz
+            dlg(iz)=(z(iz)+z(iz+1))/2.;
+        end
+
+        for iz=1:nz
+            dld(iz)=min(dld(iz),dlg(iz));
+            dls(iz)=sqrt(dlu(iz)*dld(iz));
+            dlk(iz)=min(dlu(iz),dld(iz));
+        end
+
     end
 
-    for iz=1:nz
-        dld(iz)=min(dld(iz),dlg(iz));
-        dls(iz)=sqrt(dlu(iz)*dld(iz));
-        dlk(iz)=min(dlu(iz),dld(iz));
-    end
-
-end
-
-"""
-
-def invert(nz,A,C):
-    """
-    Inversion and resolution of a tridiagonal matrix
-             A X = C
-    Input:
-     nz number of layers
-     a(*,1) lower diagonal (Ai,i-1)
-     a(*,2) principal diagonal (Ai,i)
-     a(*,3) upper diagonal (Ai,i+1)
-     c
-    Output
-     x     results
     """
 
-    X = [0 for i in xrange(nz)]
+    def invert(self,nz,A,C):
+        """
+        Inversion and resolution of a tridiagonal matrix
+                 A X = C
+        Input:
+         nz number of layers
+         a(*,1) lower diagonal (Ai,i-1)
+         a(*,2) principal diagonal (Ai,i)
+         a(*,3) upper diagonal (Ai,i+1)
+         c
+        Output
+         x     results
+        """
 
-    for i in reversed(xrange(nz-1)):
-        C[i] = C[i] - A[i][2] * C[i+1]/A[i+1][1]
-        A[i][1] = A[i][1] - A[i][2] * A[i+1][0]/A[i+1][1]
+        X = [0 for i in xrange(nz)]
 
-    for i in  xrange(1,nz,1):
-        C[i] = C[i] - A[i][0] * C[i-1]/A[i-1][1]
+        for i in reversed(xrange(nz-1)):
+            C[i] = C[i] - A[i][2] * C[i+1]/A[i+1][1]
+            A[i][1] = A[i][1] - A[i][2] * A[i+1][0]/A[i+1][1]
 
-    for i in xrange(nz):
-        X[i] = C[i]/A[i][1]
+        for i in  xrange(1,nz,1):
+            C[i] = C[i] - A[i][0] * C[i-1]/A[i-1][1]
 
-    return X
+        for i in xrange(nz):
+            X[i] = C[i]/A[i][1]
+
+        return X
