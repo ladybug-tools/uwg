e2aee23b66eea2aa7be38ad2b0163da5790f8daa
Saeran Vasanthakumar <saeranv@gmail.com>
2020-08-25 11:05:46 -0400

    feat(uwg): Add CI docs.

--- setup.py
+++ setup.py
@@ -1,40 +1,28 @@
-import re
 import setuptools
-import sys
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
-with open('uwg/__init__.py', 'r') as fd:
-    version = re.search(
-        r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
-        fd.read(),
-        re.MULTILINE
-    ).group(1)
-
-try:
-    from semantic_release import setup_hook
-    setup_hook(sys.argv)
-except ImportError:
-    pass
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
 
 setuptools.setup(
     name="uwg",
-    version=version,
-    author="ladybug-tools",
-    description="The Urban Weather Generator engine for Urban Heat Island modelling",
+    use_scm_version=True,
+    setup_requires=['setuptools_scm'],
+    author="Ladybug Tools",
+    author_email="info@ladybug.tools",
+    description="The Urban Weather Generator (uwg) is a Python application for modeling the urban heat island effect.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/ladybug-tools/uwg",
-    packages=['uwg'],
-    include_package_data=True,
+    packages=setuptools.find_packages(exclude=["tests*"]),
+    install_requires=requirements,
     classifiers=[
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3.6",
-        "Operating System :: OS Independent",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent"
     ],
 )
