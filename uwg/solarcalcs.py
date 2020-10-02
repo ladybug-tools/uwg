0a1601ac234ba2d006b1531712480c935646c0d4
Saeran Vasanthakumar <saeranv@gmail.com>
2020-10-01 23:19:53 -0400

    feat(solarcalcs): PEP8 conversion of solarcalcs.

--- uwg/solarcalcs.py
+++ uwg/solarcalcs.py
@@ -69,7 +69,7 @@ class SolarCalcs(object):
         self.dir = self.forc.dir  # Direct sunlight (perpendicular to the sun's ray)
         self.dif = self.forc.dif  # Diffuse sunlight
 
-        if self.dir + self.dif > 0.:
+        if (self.dir + self.dif) > 0.:
 
             self.logger.debug("{} Solar radiation > 0".format(__name__))
 
@@ -77,20 +77,26 @@ class SolarCalcs(object):
             self.solarangles()
 
             # Direct horizontal radiation
-            self.horSol = max(math.cos(self.zenith)*self.dir, 0.0)
+            self.horSol = max(math.cos(self.zenith) * self.dir, 0.0)
             # Fractional terms for wall & road
-            self.Kw_term = min(abs(1./self.UCM.canAspect*(0.5-self.critOrient/math.pi) \
-                + 1/math.pi*self.tanzen*(1-math.cos(self.critOrient))),1.)
-            self.Kr_term = min(abs(2.*self.critOrient/math.pi \
-                - (2/math.pi*self.UCM.canAspect*self.tanzen)*(1-math.cos(self.critOrient))), 1-2*self.UCM.canAspect*self.Kw_term)
-
+            self.Kw_term = (
+                min(abs(1. / self.UCM.canAspect*(0.5 - self.critOrient / math.pi) + 1 /
+                    math.pi * self.tanzen * (1 - math.cos(self.critOrient))), 1.))
+            self.Kr_term = (
+                min(abs(2. * self.critOrient / math.pi -
+                    (2 / math.pi * self.UCM.canAspect * self.tanzen) *
+                    (1 - math.cos(self.critOrient))),
+                    1 - 2 * self.UCM.canAspect * self.Kw_term))
 
             # Direct and diffuse solar radiation
-            self.bldSol = self.horSol*self.Kw_term + self.UCM.wallConf*self.dif   # Assume trees are shorter than buildings
-            self.roadSol = self.horSol*self.Kr_term + self.UCM.roadConf*self.dif
+            # Assume trees are shorter than buildings
+            self.bldSol = self.horSol * self.Kw_term + self.UCM.wallConf * self.dif
+            self.roadSol = self.horSol * self.Kr_term + self.UCM.roadConf * self.dif
 
-            # Solar reflections. Add diffuse radiation from vegetation to alb_road if in season
-            if self.simTime.month < self.parameter.vegStart or self.simTime.month > self.parameter.vegEnd:
+            # Solar reflections. Add diffuse radiation from vegetation to alb_road if
+            # in season
+            if self.simTime.month < self.parameter.vegStart or \
+               self.simTime.month > self.parameter.vegEnd:
                 alb_road = self.UCM.road.albedo
             else:
                 alb_road = self.UCM.road.albedo * (1. - self.UCM.road.vegcoverage) + \
@@ -107,16 +113,19 @@ class SolarCalcs(object):
                 self.UCM.alb_wall)
 
             # (1.0-self.UCM.roadConf) road to wall view
-            self.mr = (rr + (1.0-self.UCM.roadConf) * alb_road * (rw + self.UCM.wallConf * self.UCM.alb_wall * rr)) / fr
+            self.mr = (rr + (1.0 - self.UCM.roadConf) * alb_road *
+                       (rw + self.UCM.wallConf * self.UCM.alb_wall * rr)) / fr
             self.mw = (rw + self.UCM.wallConf * self.UCM.alb_wall * rr) / fr
 
             # Receiving solar, including bounces (W m-2)
             # Includes road covered by vegetation.
-            self.UCM.road.solRec = self.roadSol + (1 - self.UCM.roadConf)*self.mw
+            self.UCM.road.solRec = self.roadSol + (1 - self.UCM.roadConf) * self.mw
 
             for j in range(len(self.BEM)):
                 self.BEM[j].roof.solRec = self.horSol + self.dif
-                self.BEM[j].wall.solRec = self.bldSol + (1 - 2*self.UCM.wallConf) * self.mw + self.UCM.wallConf * self.mr
+                self.BEM[j].wall.solRec = (
+                    self.bldSol + (1 - 2*self.UCM.wallConf)*self.mw +
+                    self.UCM.wallConf * self.mr)
 
             self.rural.solRec = self.horSol + self.dif  # Solar received by rural
             self.UCM.SolRecRoof = self.horSol + self.dif  # Solar received by roof
@@ -190,48 +199,57 @@ class SolarCalcs(object):
 
         month = self.simTime.month
         day = self.simTime.day
-        secDay = self.simTime.secDay    # Total elapsed seconds in simulation
-        inobis = self.simTime.inobis    # total days for first of month
-                                        #  i.e [0,31,59,90,120,151,181,212,243,273,304,334]
+        secDay = self.simTime.secDay  # Total elapsed seconds in simulation
+        # inobis: total days for first of month
+        # i.e [0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334]
+        inobis = self.simTime.inobis
+
         canAspect = self.UCM.canAspect
         lon = self.RSM.lon
         lat = self.RSM.lat
         GMT = self.RSM.gmt
 
-        self.ut = (24. + (int(secDay)/3600.%24.)) % 24. # Get elapsed hours on current day
+        # Get elapsed hours on current day
+        self.ut = (24. + (int(secDay) / 3600. % 24.)) % 24.
         ibis = list(range(len(inobis)))
 
         for JI in range(1, 12):
-             ibis[JI] = inobis[JI]+1
+            ibis[JI] = inobis[JI] + 1
 
-        date = day + inobis[month-1]-1 # Julian day of the year
+        date = day + inobis[month-1] - 1  # Julian day of the year
         # divide circle by 365 days, multiply by elapsed days + hours
-        self.ad = 2.0 * math.pi/365. * (date-1 + (self.ut-(12/24.)))     # Fractional year (radians)
+        # ad: fractional year (radians)
+        self.ad = 2.0 * math.pi / 365. * (date - 1 + (self.ut - (12 / 24.)))
 
-        self.eqtime = 229.18 * (0.000075+0.001868*math.cos(self.ad)-0.032077*math.sin(self.ad) - \
-            0.01461*math.cos(2*self.ad)-0.040849*math.sin(2*self.ad))
+        self.eqtime = (
+            229.18 * (0.000075 + 0.001868 * math.cos(self.ad) - 0.032077 *
+                      math.sin(self.ad) - 0.01461 * math.cos(2 * self.ad) - 0.040849 *
+                      math.sin(2 * self.ad)))
 
         # Declination angle (angle of sun with equatorial plane)
-        self.decsol = 0.006918-0.399912*math.cos(self.ad)+0.070257*math.sin(self.ad) \
-            -0.006758*math.cos(2.*self.ad)+0.000907*math.sin(2.*self.ad) \
-            -0.002697*math.cos(3.*self.ad)+0.00148 *math.sin(3.*self.ad)
+        self.decsol = (
+            0.006918 - 0.399912*math.cos(self.ad) + 0.070257*math.sin(self.ad) -
+            0.006758 * math.cos(2. * self.ad) + 0.000907 * math.sin(2. * self.ad) -
+            0.002697 * math.cos(3. * self.ad) + 0.00148 * math.sin(3. * self.ad))
 
         time_offset = self.eqtime - 4. * lon + 60 * GMT
         tst = secDay + time_offset * 60
 
-        ha = (tst/4./60.-180.) * math.pi/180.
-        zlat = lat * (math.pi/180.)   # change angle units to radians
+        ha = (tst / 4. / 60. - 180.) * math.pi / 180.
+        zlat = lat * (math.pi / 180.)  # change angle units to radians
 
         # Calculate zenith solar angle
-        self.zenith = math.acos(math.sin(zlat)*math.sin(self.decsol) + math.cos(zlat)*math.cos(self.decsol)*math.cos(ha))
+        self.zenith = (
+            math.acos(math.sin(zlat) * math.sin(self.decsol) + math.cos(zlat) *
+                      math.cos(self.decsol) * math.cos(ha)))
 
         # tangente of solar zenithal angle
-        if abs(0.5*math.pi - self.zenith) < 1e-6:
-            if 0.5*math.pi - self.zenith > 0.:
-                self.tanzen = math.tan(0.5*math.pi-1e-6)
+        if abs(0.5 * math.pi - self.zenith) < 1e-6:
+            if 0.5 * math.pi - self.zenith > 0.:
+                self.tanzen = math.tan(0.5 * math.pi - 1e-6)
 
-            elif 0.5*math.pi - self.zenith <= 0.:
-                self.tanzen = math.tan(0.5*math.pi+1e-6)
+            elif 0.5 * math.pi - self.zenith <= 0.:
+                self.tanzen = math.tan(0.5 * math.pi + 1e-6)
 
         elif abs(self.zenith) < 1e-6:
             # lim x->0 tan(x) -> 0 which results in division by zero error
@@ -244,4 +262,4 @@ class SolarCalcs(object):
 
         # critical canyon angle for which solar radiation reaches the road
         # has to do with street canyon orientation for given solar angle
-        self.critOrient = math.asin(min(abs( 1./self.tanzen)/canAspect, 1. ))
+        self.critOrient = math.asin(min(abs(1. / self.tanzen) / canAspect, 1.))
